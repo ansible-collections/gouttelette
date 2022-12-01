4659e481c222b5c2a71a5c53472269433cdd5fbf
Gonéri Le Bouder <goneri@lebouder.net>
2022-12-01 12:41:54 -0500

    import jinja2_renderer()

---
+++ gouttelette/utils.py
@@ -0,0 +1,8 @@
+def jinja2_renderer(template_file, **kwargs):
+    templateLoader = jinja2.PackageLoader("amazon_cloud_code_generator")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(template_file)
+    return template.render(kwargs)
+
+
+
