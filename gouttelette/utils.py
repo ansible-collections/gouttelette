60d5c583f84ec7d1c6b526e6e91c08a10248af09
GomathiselviS <gomathiselvi@gmail.com>
2022-12-01 13:13:27 -0500

    format using black

--- gouttelette/utils.py
+++ gouttelette/utils.py
@@ -1,8 +1,5 @@
-def jinja2_renderer(template_file, **kwargs):
-    templateLoader = jinja2.PackageLoader("amazon_cloud_code_generator")
+def jinja2_renderer(template_file, generator, **kwargs):
+    templateLoader = jinja2.PackageLoader(generator)
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(template_file)
     return template.render(kwargs)
-
-
-
