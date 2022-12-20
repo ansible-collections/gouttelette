7e0384d932aace448e1f2b2eb10fa87e5f2488a0
GomathiselviS <gomathiselvi@gmail.com>
2022-12-20 11:16:23 -0500

    Removing colelction specific files

--- gouttelette/cmd/refresh_modules.py
+++ gouttelette/cmd/refresh_modules.py
@@ -1223,7 +1223,7 @@ def generate_amazon_cloud(args: Iterable):
         yaml.safe_dump(yaml_dict, file, sort_keys=False)
 
     collection_dir = pkg_resources.resource_filename("gouttelette", "data")
-    print(f"Copying collection from {collection_dir}")
+    print(f"Copying files from {collection_dir}")
     shutil.copytree(collection_dir, args.target_dir, dirs_exist_ok=True)
 
     return
